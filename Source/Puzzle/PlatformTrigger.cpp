eaf9bbac771721360fd9de36217ca66986af9e80
asdf37846 <89976244+asdf37846@users.noreply.github.com>
2023-07-17 18:29:28 +0900

    OverlapEvent
    C++에서 BeginOverlap와 EndOverlap을 이용하여 로그 출력

--- Source/Puzzle/PlatformTrigger.cpp
+++ Source/Puzzle/PlatformTrigger.cpp
@@ -14,14 +14,19 @@ APlatformTrigger::APlatformTrigger()
 	if (!ensure(TriggerVolume != nullptr))return;
 
 	RootComponent = TriggerVolume;
-	
+
 }
 
 // Called when the game starts or when spawned
 void APlatformTrigger::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	if (!ensure(TriggerVolume != nullptr))return;
+
+	TriggerVolume->OnComponentBeginOverlap.AddDynamic(this, &APlatformTrigger::OnOverlapBegin);
+
+	TriggerVolume->OnComponentEndOverlap.AddDynamic(this, &APlatformTrigger::OnOverlapEnd);
 }
 
 // Called every frame
@@ -29,5 +34,15 @@ void APlatformTrigger::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void APlatformTrigger::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Activated"));
+}
+
+void APlatformTrigger::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Deactivated"));
 }
 
