6c6542a5dd246822456b3b28f6d8fbb99af7b724
asdf37846 <89976244+asdf37846@users.noreply.github.com>
2023-07-19 18:25:29 +0900

    ActiveTrigger
    PlatformTrigger에서 플랫폼을 정하고 발판을 밟아서 플랫폼 작동시키는 기능 완성

--- Source/Puzzle/MovingPlatform.cpp
+++ Source/Puzzle/MovingPlatform.cpp
@@ -29,21 +29,38 @@ void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	if (HasAuthority())
+	if (ActiveTriggers > 0)
 	{
-		FVector Location = GetActorLocation();
-		float MoveLengh = (GlobalTargetLocation - GlobalStartLocation).Size();
-		float MoveTravelled = (Location - GlobalStartLocation).Size();
-
-		if (MoveTravelled >= MoveLengh)
+		if (HasAuthority())
 		{
-			FVector Swap = GlobalStartLocation;
-			GlobalStartLocation = GlobalTargetLocation;
-			GlobalTargetLocation = Swap;
-		}
+			FVector Location = GetActorLocation();
+			float MoveLengh = (GlobalTargetLocation - GlobalStartLocation).Size();
+			float MoveTravelled = (Location - GlobalStartLocation).Size();
 
-		FVector Direction = (GlobalTargetLocation - GlobalStartLocation).GetSafeNormal();
-		Location += Speed * DeltaTime * Direction;
-		SetActorLocation(Location);
+			if (MoveTravelled >= MoveLengh)
+			{
+				FVector Swap = GlobalStartLocation;
+				GlobalStartLocation = GlobalTargetLocation;
+				GlobalTargetLocation = Swap;
+			}
+
+			FVector Direction = (GlobalTargetLocation - GlobalStartLocation).GetSafeNormal();
+			Location += Speed * DeltaTime * Direction;
+			SetActorLocation(Location);
+		}
+	}
+	
+}
+
+void AMovingPlatform::AddActiveTrigger()
+{
+	ActiveTriggers++;
+}
+
+void AMovingPlatform::RemoveActiveTrigger()
+{
+	if (ActiveTriggers > 0)
+	{
+		ActiveTriggers--;
 	}
 }
--- Source/Puzzle/PlatformTrigger.cpp
+++ Source/Puzzle/PlatformTrigger.cpp
@@ -4,6 +4,8 @@
 #include "PlatformTrigger.h"
 #include "Components/BoxComponent.h"
 
+#include "MovingPlatform.h"
+
 // Sets default values
 APlatformTrigger::APlatformTrigger()
 {
@@ -38,11 +40,17 @@ void APlatformTrigger::Tick(float DeltaTime)
 
 void APlatformTrigger::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Activated"));
+	for (AMovingPlatform* Platform : PlatformToTrigger)
+	{
+		Platform->AddActiveTrigger();
+	}
 }
 
 void APlatformTrigger::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Deactivated"));
+	for (AMovingPlatform* Platform : PlatformToTrigger)
+	{
+		Platform->RemoveActiveTrigger();
+	}
 }
 
